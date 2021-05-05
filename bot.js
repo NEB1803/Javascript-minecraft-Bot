42b84d22f8457adac952072b2ea4f53979b8eb88
NEB1803 <83712440+NEB1803@users.noreply.github.com>
2021-05-05 19:38:40 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@
                         s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
                     }
                 });
-                s4d.client.login('Nzc0NTQxNDU5NzMwMDcxNTUy.X6ZR8g.B_ja_5L4cQWzfVaNCPBYHXFagLA').catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+                s4d.client.login(process.env.TOKEN).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
 
 s4d.client.on('ready', async () => {
 
